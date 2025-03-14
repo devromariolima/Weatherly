bac94ddcb2ea33ff170cbcc7655f310d0bb06d91
Romário Lima <devromariolima@gmail.com>
2025-03-13 21:50:15 -0400

    Updates the display of weather data and improves the error message for cities not found

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const ApiKey = '95f7c93536c7e386c6c5a0b04247e7dd';
 
+function insertData(data) {
+    document.querySelector('.city').innerText = "Tempo em: " + data.name;
+    document.querySelector('.temp').innerText = data.main.temp + " °C";
+    document.querySelector('.text-prev').innerText = data.weather[0].description;
+    document.querySelector('.humidity').innerText = 'Umidade: ' + data.main.humidity + ' %';
+
+}
+
 async function getCity(input) {
 
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${ApiKey}`)
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${ApiKey}&lang=pt_br&units=metric`)
         .then(response => response.json())
 
+    if (data.cod == 404) {
+        return alert('Cidade não encontrada')
+    }
+
+    insertData(data)
     console.log(data)
 }
 
